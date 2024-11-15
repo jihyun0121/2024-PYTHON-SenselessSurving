c472d9ed933238da2da5ab587ea371130bc96dfc
gheun <gheun098@gmail.com>
2024-11-15 13:42:47 +0900

    ✨[feat] 타이포효과 추가

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import random
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget, \
     QListWidgetItem, QMessageBox
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QTimer
 
 STORY_DATA_FILE = 'assets/story_data.json'
+
 
 def load_story_data():
     with open(STORY_DATA_FILE, 'r', encoding='utf-8') as f:
@@ -17,54 +18,86 @@ class GameWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.item_label = None
         self.title_layout = None
         self.title_image_label = None
         self.start_button = None
 
-        self.setWindowTitle("비상식에서 살아남기")
-        self.setGeometry(100, 100, 800, 450)  #크기 비율
-        self.setMinimumSize(700, 550)   #최소 사이즈
+        self.lives_container = None
+        self.status_layout = None
+        self.lives_layout = None
+        self.lives_label = None
 
-        #초기 리스트, 배열 설정
+        self.sense_container = None
+        self.sense_layout = None
+        self.sense_label = None
+
+        self.money_container = None
+        self.money_layout = None
+        self.money_label = None
+
+        self.final_label = None
+
+        self.story_image_label = None
+        self.story_text_label = None
+        self.choice_buttons_layout = None
+        self.show_items_button = None
+
+        self.current_category = None
+        self.show_items_button = None
+        self.reset_button = None
+
+        self.setWindowTitle("비상식에서 살아남기")
+        self.setGeometry(100, 100, 800, 450)
+        self.setMinimumSize(700, 550)
+
+        # 초기 리스트, 배열 설정
         self.scenes = []
         self.meet = []
         self.status = {
             "lives": 3,
             "sense": 3,
             "money": 3,
-            "found_treasures": 0
+            "end_correct": 0
         }
 
         self.setStyleSheet(open("assets/styles/style.qss").read())
 
         self.layout = QVBoxLayout()
 
-        #게임 시작 시 타이틀 화면 표시
+        # 게임 시작 시 타이틀 화면 표시
         self.show_title()
 
         self.setLayout(self.layout)
 
-        #스토리 데이터 불러오기
+        # 스토리 데이터를 불러옴
         self.story_data = load_story_data()
         self.load_scenes()
 
-        self.item_list = QListWidget()
+        self.item_list = QListWidget()  # 아이템 목록
         self.layout.addWidget(self.item_list)
         self.item_list.hide()
 
-    def load_scenes(self):  #배열에 자동(동적) 추가
+        self.current_text = ""
+        self.text_index = 0
+        self.typing_timer = QTimer()
+        self.typing_timer.timeout.connect(self.update_typing_effect)
+
+    def load_scenes(self):
+        # scenes 자동(동적) 추가
         self.scenes = [key for key in self.story_data if key.startswith('scenes')]
         self.scenes += [key for key in self.story_data if key.startswith('trader')]
         self.scenes += [key for key in self.story_data if key.startswith('meet')]
 
     def show_item_list(self):
+        # 아이템 목록을 보이거나 숨김
         if self.item_list.isVisible():
             self.item_list.hide()
         else:
             self.item_list.show()
 
     def show_title(self):
-        #모든 위젯 제거
+        # 모든 위젯 제거
         if self.title_layout is not None:
             for i in reversed(range(self.title_layout.count())):
                 widget = self.title_layout.itemAt(i).widget()
@@ -73,7 +106,7 @@ class GameWindow(QWidget):
 
         self.title_layout = QVBoxLayout()
 
-        #제목, 게임 시작 추가
+        # 제목, 게임 시작 추가
         self.title_image_label = QLabel(self)
         pixmap = QPixmap('assets/images/title/title.png')
         self.title_image_label.setPixmap(pixmap.scaled(300, 200, Qt.AspectRatioMode.KeepAspectRatio))
@@ -90,7 +123,7 @@ class GameWindow(QWidget):
             "lives": 3,
             "sense": 3,
             "money": 3,
-            "found_treasures": 0
+            "end_correct": 0
         }
 
         self.title_image_label.hide()  # 타이틀, 시작 버튼 숨기기
@@ -118,7 +151,7 @@ class GameWindow(QWidget):
         self.money_label = QLabel()
         self.money_layout.addWidget(self.money_label)
 
-        self.final_label = QLabel("스택: 0")
+        self.final_label = QLabel("엔딩: 0")
 
         self.status_layout.addWidget(self.lives_container)
         self.status_layout.addWidget(self.sense_container)
@@ -127,27 +160,27 @@ class GameWindow(QWidget):
 
         self.layout.addLayout(self.status_layout)
 
-        # 스토리 이미지 표시
+        # 스토리 이미지
         self.story_image_label = QLabel(self)
         self.layout.addWidget(self.story_image_label)
 
-        # 스토리 텍스트 표시
+        # 스토리 텍스트
         self.story_text_label = QLabel("텍스트 표시", self)
         self.story_text_label.setWordWrap(True)
         self.layout.addWidget(self.story_text_label)
 
-        # 선택지 버튼 레이아웃
+        # 선택지 버튼
         self.choice_buttons_layout = QVBoxLayout()
         self.layout.addLayout(self.choice_buttons_layout)
 
-        self.item_list = QListWidget()  # 여기에서 초기화
+        self.item_list = QListWidget()
 
         self.show_items_button = QPushButton("아이템 보기")
         self.show_items_button.setFixedSize(100, 50)
         self.show_items_button.clicked.connect(self.show_item_list)
         self.layout.addWidget(self.show_items_button)
 
-        #초기화
+        # 초기화
         self.reset_button = QPushButton("초기화")
         self.reset_button.setFixedSize(100, 50)
         self.reset_button.clicked.connect(self.reset_game)
@@ -162,7 +195,7 @@ class GameWindow(QWidget):
         try:
             data = self.story_data[category][scene_name]
 
-            self.story_text_label.setText(data['text'])
+            self.start_typing_effect(data['text'])  # 타이핑 효과 적용
 
             if data.get('image'):
                 pixmap = QPixmap(data['image']).scaled(400, 200, Qt.AspectRatioMode.KeepAspectRatio)
@@ -170,7 +203,7 @@ class GameWindow(QWidget):
             else:
                 self.story_image_label.clear()
 
-            # 선택지 버튼을 리셋
+            # 선택지 버튼 리셋
             self.reset_choice()
 
             # 새로운 선택지 버튼 추가
@@ -188,17 +221,15 @@ class GameWindow(QWidget):
             QMessageBox.critical(self, "에러", f"장면 '{scene_name}' 또는 카테고리 '{category}'을(를) 찾을 수 없습니다. 오류: {e}")
 
     def update_status(self, status):
-        # 기본 상태 값
         lives = status.get('lives', self.status['lives'])
         sense = status.get('sense', self.status['sense'])
         money = status.get('money', self.status['money'])
-        found_treasures = status.get('found_treasures', self.status['found_treasures'])
+        end_correct = status.get('end_correct', self.status['end_correct'])
 
-        # 개별 레이블 업데이트
         self.lives_label.setText(f"목숨: {'❤️' * lives}")
         self.sense_label.setText(f"멘탈: {'📝' * sense}")
         self.money_label.setText(f"돈: {'💰' * money}")
-        self.final_label.setText(f"보물: {'✨' * found_treasures}")
+        self.final_label.setText(f"상식: {'✨' * end_correct}")
 
     def reset_choice(self):
         for i in reversed(range(self.choice_buttons_layout.count())):
@@ -241,7 +272,7 @@ class GameWindow(QWidget):
             "lives": 3,
             "sense": 3,
             "money": 3,
-            "found_treasures": 0
+            "end_correct": 0
         }
 
         # 남은 scenes 리스트 초기화
@@ -281,8 +312,6 @@ class GameWindow(QWidget):
         matching_items = [self.item_list.item(i) for i in range(self.item_list.count()) if
                           items in self.item_list.item(i).text()]
 
-        print(f"현재 감소된 아이템: {items}")  # 배열 확인용
-
         if matching_items:
             current_item = matching_items[0]
             current_text = current_item.text()
@@ -293,70 +322,56 @@ class GameWindow(QWidget):
                 current_item.setText(f"{items} x {new_count}")
             else:
                 self.item_list.takeItem(self.item_list.row(current_item))
-            return True  # 아이템이 충분하여 성공적으로 감소함
+            return True
         else:
             self.show_message(f"{items} 부족")
-            return False  # 아이템이 부족하여 화면 전환을 막기 위해 False 반환
+            return False
 
     def get_Ability(self, ability):
         item_dict = {}
 
-        print(f"현재 추가된 능력: {ability}")  # 배열 확인용
-
-        # 아이템 개수 세기
         for item in ability:
             if item in item_dict:
                 item_dict[item] += 1
             else:
                 item_dict[item] = 1
 
-        # 리스트에 있는 아이템 업데이트
         for i in range(self.item_list.count()):
             item_text = self.item_list.item(i).text()
             item_name = item_text.split(" lv. ")[0]
             if item_name in item_dict:
-                new_count = item_dict[ability] + int(item_text.split(" x ")[1])  # 기존 개수에 더함
+                new_count = item_dict[ability] + int(item_text.split(" x ")[1])
                 self.item_list.item(i).setText(f"{ability} x {new_count}")
-                del item_dict[ability]  # 이미 처리한 아이템은 삭제
+                del item_dict[ability]
 
-        # 새로운 아이템 추가
         for item, count in item_dict.items():
-            if count > 0:  # 개수가 0보다 클 때만 추가
+            if count > 0:
                 list_item = QListWidgetItem(f"{item} lv. {count}")
                 self.item_list.addItem(list_item)
 
     def lose_Ability(self, ability):
-        # 현재 아이템 목록에서 이름에 해당하는 아이템을 찾기
         matching_items = [self.item_list.item(i) for i in range(self.item_list.count()) if
                           ability in self.item_list.item(i).text()]
-
-
-        print(f"현재 감소된 능력: {ability}")  # 배열 확인용
 
         if matching_items:
             current_item = matching_items[0]
             current_text = current_item.text()
 
-            # 현재 아이템 개수 추출
             current_count = int(current_text.split(" lv. ")[1])
 
-            # 개수를 1 감소
             new_count = current_count - 1
 
             if new_count > 0:
-                # 아이템 개수를 업데이트
                 current_item.setText(f"{ability} lv. {new_count}")
             else:
-                # 아이템 개수가 0이면 아이템 삭제
                 self.item_list.takeItem(self.item_list.row(current_item))
         else:
-            self.show_message(f"{ability}이 부족합니다!")
-            return False  # 아이템이 부족하여 화면 전환을 막기 위해 False 반환
+            self.show_message(f"{ability} 부족")
+            return False
 
     def make_choice(self, next_scene, action_id):
         current_category = self.current_category
         Q_correct = 0
-        corrects = 0
 
         action_id = [id.strip() for id in action_id.split(",")]
 
@@ -382,7 +397,7 @@ class GameWindow(QWidget):
             elif id == "lose_money":
                 if self.status['money'] == 0:
                     self.show_message("돈이 부족합니다!")
-                    return  # 화면 전환을 막기 위해 return
+                    return
                 else:
                     self.status['money'] -= 1
 
@@ -422,25 +437,60 @@ class GameWindow(QWidget):
                 if not self.lose_items("책"):
                     return
 
+            elif id == "get_cake":
+                self.get_items(["케이크"])
+            elif id == "lose_cake":
+                if not self.lose_items("케이크"):
+                    return
+
+            elif id == "get_healthy":
+                self.get_Ability(["근력"])
+            elif id == "lose_healthy":
+                if not self.lose_Ability("근력"):
+                    return
+
+            elif id == "get_intuition":
+                self.get_Ability(["강력한 직감"])
+            elif id == "lose_intuition":
+                if not self.lose_Ability("강력한 직감"):
+                    return
+
+            elif id == "get_handmade":
+                self.get_Ability(["손재주"])
+            elif id == "lose_handmade":
+                if not self.lose_Ability("손재주"):
+                    return
+
+            elif id == "get_musician":
+                self.get_Ability(["음악적 재능"])
+            elif id == "lose_musician":
+                if not self.lose_Ability("음악적 재능"):
+                    return
+
+            elif id == "get_emergency":
+                self.get_Ability(["응급치료술"])
+            elif id == "lose_emergency":
+                if not self.lose_Ability("응급치료술"):
+                    return
+
+            elif id == "get_agile":
+                self.get_Ability(["날렵함"])
+            elif id == "lose_agile":
+                if not self.lose_Ability("날렵함"):
+                    return
+
             elif id == "get_lock":
                 self.get_Ability(["자물쇠 따기"])
             elif id == "lose_lock":
                 if not self.lose_Ability("자물쇠 따기"):
                     return
 
-            elif id == "get_rice":
-                self.get_items(["케이크"])
-            elif id == "lose_rice":
-                if not self.lose_items("케이크"):
-                    return
-
             elif id == "quiz":
                 Q_correct = 0
             elif id == "correct":
                 Q_correct += 1
-            elif Q_correct >= 4:
-                corrects += 1
-                Q_correct = 0
+                if Q_correct == 3:
+                    self.status['end_correct'] += 1
 
         # 상태 업데이트
         self.update_status(self.status)
@@ -460,11 +510,13 @@ class GameWindow(QWidget):
         elif next_scene == "start_game":
             self.reset_game()
             self.show_title()
-        elif next_scene == "end_game" or len(self.scenes) < 0:
-            if corrects >= 3:
-                self.display_scene("ending", "ending0-1")
+        elif next_scene == "end_game" or len(self.scenes) == 0:
+            if self.status['end_correct'] >= 3:
+                self.display_scene("ending", "ending3")
+            elif self.status['end_corrext'] > 0 and self.status['end_corrext'] < 3:
+                self.display_scene("ending", "ending3")
             else:
-                self.display_scene("ending", "ending0-2")
+                self.display_scene("ending", "ending1")
         elif next_scene == "quit":
             QApplication.quit()
         else:
@@ -476,9 +528,21 @@ class GameWindow(QWidget):
         msg.setText(message)
         msg.setWindowTitle("경고")
         msg.exec()
+        
+    def start_typing_effect(self, text):
+        self.current_text = text
+        self.text_index = 0
+        self.story_text_label.setText("")  # 텍스트 초기화
+        self.typing_timer.start(20)  # 타이머 설정 (50초 마다 글자 추가)
 
+    def update_typing_effect(self):
+        if self.text_index < len(self.current_text):
+            # 현재 인덱스 위치의 글자를 추가
+            self.story_text_label.setText(self.story_text_label.text() + self.current_text[self.text_index])
+            self.text_index += 1
+        else:
+            self.typing_timer.stop()  # 텍스트가 모두 출력되면 타이머 중지
 def main():
-    init_db()
     app = QApplication(sys.argv)
     window = GameWindow()
     window.show()
